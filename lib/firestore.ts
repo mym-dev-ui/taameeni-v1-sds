32d54203d0b27b3e5c421c3f537404b59ee3168a
mym-dev-ui <saddam.saqqar@icloud.com>
2026-05-19 09:03:19 +0300

    Update firestore.ts

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -4,15 +4,15 @@ import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjeoP0ia224jtc8U7JCoIYOHJcudBardo",
-  authDomain: "myapp-58e0a.firebaseapp.com",
-  databaseURL: "https://myapp-58e0a-default-rtdb.firebaseio.com",
-  projectId: "myapp-58e0a",
-  storageBucket: "myapp-58e0a.appspot.com",
-  messagingSenderId: "835173729732",
-  appId: "1:835173729732:web:04b9a1f75b4c7837641091"
+  apiKey: "AIzaSyB-qlb_QYAPBqijr00XN-PeUd9DTzI0MDs",
+  authDomain: "taameeni-v1.firebaseapp.com",
+  databaseURL: "https://taameeni-v1-default-rtdb.firebaseio.com",
+  projectId: "taameeni-v1",
+  storageBucket: "taameeni-v1.firebasestorage.app",
+  messagingSenderId: "240999338900",
+  appId: "1:240999338900:web:bb73a1ea1239d2c074f581",
+  measurementId: "G-MP49WZ65T2"
 };
-
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
